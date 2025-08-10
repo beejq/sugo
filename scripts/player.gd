33ce958edacce6c0d3572cc7c02f5f24719ae7d7
beejq <bienjulius0@gmail.com>
2025-08-10 20:23:24 +0800

    Dash added

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,24 +3,32 @@ extends CharacterBody2D
 
 const SPEED = 60.0
 const JUMP_VELOCITY = -140.0
-var previous_dir: int
+
+var previous_dir: int = 1
+var dashCD: bool = false
+var dashingPlayer: bool = false
+var dashDirection: int
+var dashMul
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var dash_duration: Timer = $dashDuration
+@onready var dash_cd: Timer = $dashCD
 
 func _physics_process(delta: float) -> void:
+	var dashMultiplicator = 3.5
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	
-	#Animation for Walking (Left or Right)
+	# Animation for Walking (Left or Right) as well as Logic for Walking
 	if direction == 0:
 		if previous_dir < 0:
 			animated_sprite_2d.play("left-idle")
@@ -35,5 +43,27 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.play("left-walk")
 		velocity.x = direction * SPEED
 		previous_dir = direction
-
+		
+	# Handle dash.
+	if Input.is_action_just_pressed("dash") and not dashCD:
+		dashDirection = 1 if previous_dir > 0 else -1
+		dashingPlayer = true
+		dashCD = true
+		dash_duration.start()
+		dash_cd.start()
+	
+	if dashingPlayer:
+		velocity.x = dashDirection * SPEED * dashMultiplicator
+		velocity.y = 0
+	elif direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	move_and_slide()
+
+func _on_dash_duration_timeout() -> void:
+	dashingPlayer = false
+
+func _on_dash_cd_timeout() -> void:
+	dashCD = false
