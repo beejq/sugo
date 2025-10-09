b5dd62df82ad5add3209c78fcdbd7fcff3f3e3fa
beejq <bienjulius0@gmail.com>
2025-10-09 09:34:31 +0800

    Added Intro Cutscene, NPC Dialogue Interact

--- scripts/intro_cutscene.gd
+++ scripts/intro_cutscene.gd
@@ -5,31 +5,32 @@ const INTRO_DIALOGUE = preload("res://dialogues/intro_dialogue.dialogue")
 @onready var cutscene_player: AnimatedSprite2D = $cutscene_player
 @onready var moving: AnimationPlayer = $Moving
 @onready var jumping: AnimationPlayer = $Jumping
+@onready var move_out: AnimationPlayer = $MoveOut
 const INTRO_2 = preload("res://dialogues/intro_2.dialogue")
+
 
 var inDialogue: bool = false
 var intro2_shown: bool = false
+var intro3_shown: bool = false
 
 func _ready() -> void:
 	await Transition.fade_out()
 	jumping.play("jumping")
-
-	jumping.animation_finished.connect(_on_jumping_finished)
 	
 	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 	
-	moving.animation_finished.connect(_on_moving_finished)
+	jumping.animation_finished.connect(_on_animation_finished)
+	
+	moving.animation_finished.connect(_on_animation_finished)
 
+	move_out.animation_finished.connect(_on_animation_finished)
+	
 func _process(delta: float) -> void:
 	if moving.is_playing():
 		cutscene_player.play("walk")
-
-func _on_jumping_finished(anim_name: String) -> void:
-	if anim_name == "jumping":
-		jumping.stop()
-		if not inDialogue:
-			inDialogue = true
-			DialogueManager.show_dialogue_balloon(INTRO_DIALOGUE, "start")
+	
+	if move_out.is_playing():
+		cutscene_player.play("walk")
 
 func _on_dialogue_ended(resource: DialogueResource) -> void:
 	if intro2_shown == false:
@@ -38,15 +39,26 @@ func _on_dialogue_ended(resource: DialogueResource) -> void:
 		moving.queue("move_in")
 	elif intro2_shown:
 		inDialogue = false
-		await get_tree().create_timer(2.0).timeout
-		Transition.fade_in()
-		get_tree().change_scene_to_file("res://scenes/game.tscn")
-					
-func _on_moving_finished(anim_name: String) -> void:
-	if anim_name == "move_in" and not intro2_shown:
+		move_out.queue("move_out")
+			
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "jumping":
+		jumping.stop()
+		if not inDialogue:
+			inDialogue = true
+			DialogueManager.show_dialogue_balloon(INTRO_DIALOGUE, "start")
+	elif anim_name == "move_in" and not intro2_shown:
 		cutscene_player.stop()
 		if not inDialogue:
 			inDialogue = true
 			intro2_shown = true
 			DialogueManager.show_dialogue_balloon(INTRO_2, "start")
+	elif anim_name == "move_out" and not intro3_shown:
+		cutscene_player.stop()
+		intro3_shown = true
+		if intro3_shown:
+			inDialogue = false
+			Transition.fade_in()
+			get_tree().change_scene_to_file("res://scenes/game.tscn")
+		
 		
